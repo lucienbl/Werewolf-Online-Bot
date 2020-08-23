441825ed49bf63c9d7f039c64aa103eaf710cae8
MainLeau <minlotiretdubas@gmail.com>
2020-08-23 22:08:04 +0200

    fix: remove canvas font

--- src/core/ClanManager.ts
+++ src/core/ClanManager.ts
@@ -1,6 +1,6 @@
 import ApiClient from "./ApiClient";
 //import { countryToEmoji } from "../utils"
-import { registerFont, createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage } from 'canvas';
 
 class ClanManager {
 
@@ -13,6 +13,7 @@ class ClanManager {
     const ctx = canvas.getContext("2d")
     const bg = await loadImage('https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg')
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height)
+    ctx.fillText(clan.name, 10, 10)
 
     return canvas.toBuffer()
   }
