9ccfecfebac2c276d1f8030584082b21a0594983
MainLeau <minlotiretdubas@gmail.com>
2020-08-25 07:26:03 +0200

    fix: help no longer shows restricted commands

--- src/core/MessageDispatcher.ts
+++ src/core/MessageDispatcher.ts
@@ -1,20 +1,3 @@
-/*
- *   Copyright (c) 2020 Lucien Blunk-Lallet
-
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
-
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
-
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 import { Message, MessageEmbed } from "discord.js";
 import * as Commands from "../commands";
 import Command from '../commands/Command';
@@ -41,7 +24,7 @@ class MessageDispatcher {
 
   private async parseAndDispatchMessage() {
     const { content } = this._message;
-  
+
     if (!content.startsWith(process.env.BOT_PREFIX)) return;
 
     const command = content.split(" ")[0].replace(process.env.BOT_PREFIX, "");
@@ -51,8 +34,8 @@ class MessageDispatcher {
         title: "List of all the commands",
         description: Object.keys(Commands).map(key => {
           const cmd: Command = new Commands[key](this._message);
-          return `• \`${cmd.usage}\` : ${cmd.description}`
-        }).join('\n')
+          return cmd.permission ? '' : `• \`${cmd.usage}\` : ${cmd.description}`
+        }).join('\n').replace(/^\s*[\r\n]/gm, "")
       }).setFooter("<required>, [optional]"));
     }
 
